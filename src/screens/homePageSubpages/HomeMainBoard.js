019a97e7fe7b797d252077a34bc400a8acd02bc1
MarcelKubicki <s091284@student.tu.kielce.pl>
2024-06-20 23:46:11 +0200

    card styles

--- src/screens/homePageSubpages/HomeMainBoard.js
+++ src/screens/homePageSubpages/HomeMainBoard.js
@@ -1,26 +1,53 @@
 import React, {useState} from 'react';
 import {Input, Avatar, Flex, Button, Typography, Card} from 'antd';
 import {UserOutlined} from '@ant-design/icons';
+import '../../styles/HomeMainBoard.css';
+
 const {Title} = Typography;
 const {Meta} = Card;
 const {TextArea} = Input;
 
+const testPosts = [
+  {
+    username: 'Xulaniec',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu orci in turpis pretium aliquam eu eu tortor. Morbi condimentum lacus vitae neque lobortis sodales. Aliquam porttitor odio metus, lacinia hendrerit nibh posuere ut. Praesent rhoncus volutpat lorem, vehicula ultricies erat convallis eget. Nunc feugiat auctor mauris, non dapibus nulla dapibus in. Phasellus vitae egestas sapien, in efficitur eros.',
+    date: '19.06.2024 15:00'
+  },
+  {
+    username: 'GochaGocha3zł',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu orci in turpis pretium aliquam eu eu tortor. Morbi condimentum lacus vitae neque lobortis sodales. Aliquam porttitor odio metus, lacinia hendrerit nibh posuere ut. Praesent rhoncus volutpat lorem, vehicula ultricies erat convallis eget. Nunc feugiat auctor mauris, non dapibus nulla dapibus in. Phasellus vitae egestas sapien, in efficitur eros.',
+    date: '19.06.2024 16:00'
+  },
+];
+
 const HomeMainBoard = ({user}) => {
   const [postContent, setPostContent] = useState('');
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(testPosts);
 
   const handlePostChange = (e) => {
     setPostContent(e.target.value);
   };
 
+  const getCurrentDate = () => {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
+  };
+
   const handlePublish = () => {
     if (postContent.trim()) {
       setPosts([
-        ...posts,
         {
           username: user ? user.username : 'User Name',
           content: postContent,
+          date: getCurrentDate(),
         },
+        ...posts
       ]);
       setPostContent('');
     }
@@ -46,12 +73,14 @@ const HomeMainBoard = ({user}) => {
             placeholder="Napisz post..."
             style={{ height: 120, resize: 'none'}}
             onChange={handlePostChange}
+            value={postContent}
           />
           <Button type='primary' onClick={handlePublish}>
             Opublikuj
           </Button>
         </Flex>
       </div>
+
       {posts.map((post, index) => (
         <Card
           key={index}
@@ -59,72 +88,24 @@ const HomeMainBoard = ({user}) => {
           style={{
             width: '100%',
             marginTop: 20,
-            boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.25)'
+            boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.25)',
+            textAlign: 'justify',
+            textJustify: 'inter-word',
           }}
         >
           <Meta
             avatar={<Avatar shape="square" size={45} icon={<UserOutlined/>} />}
             title= {post.username}
-            description= {post.content}
+            description= {post.date}
+            style={{
+              
+            }}
           />
+          <div style={{marginTop: '10px'}}>
+            {post.content}
+          </div>
         </Card>
       ))}
-      <Card
-        bordered={false}
-        style={{
-          width: '100%',
-          marginTop: 20,
-          boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.25)'
-        }}
-        // cover={
-        //   <div style={{display: 'flex', justifyContent: 'center'}}>
-        //     <img
-        //       alt="example"
-        //       src="https://nasionamarychy.pl/img/pshow/blog/118.jpg"
-        //       style={{
-        //         width: '50%',
-        //         marginTop: 20,
-        //       }}
-        //     />
-        //   </div>
-        // }
-      >
-        <Meta
-          avatar={<Avatar shape="square" size={45} icon={<UserOutlined/>} />}
-          title="User Name"
-          description="s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem"
-        />
-      </Card>
-
-      <Card
-        bordered={false}
-        style={{
-          width: '100%',
-          marginTop: 20,
-          boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.25)'
-        }}
-      >
-        <Meta
-          avatar={<Avatar shape="square" size={45} icon={<UserOutlined/>} />}
-          title="User Name"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu orci in turpis pretium aliquam eu eu tortor. Morbi condimentum lacus vitae neque lobortis sodales. Aliquam porttitor odio metus, lacinia hendrerit nibh posuere ut. Praesent rhoncus volutpat lorem, vehicula ultricies erat convallis eget. Nunc feugiat auctor mauris, non dapibus nulla dapibus in. Phasellus vitae egestas sapien, in efficitur eros. Maecenas iaculis sed velit at vehicula. Suspendisse vulputate justo placerat augue feugiat molestie. Vivamus eu feugiat augue, quis pharetra eros."
-        />
-      </Card>
-
-      <Card
-        bordered={false}
-        style={{
-          width: '100%',
-          marginTop: 20,
-          boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.25)'
-        }}
-      >
-        <Meta
-          avatar={<Avatar shape="square" size={45} icon={<UserOutlined/>} />}
-          title="User Name"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu orci in turpis pretium aliquam eu eu tortor. Morbi condimentum lacus vitae neque lobortis sodales. Aliquam porttitor odio metus, lacinia hendrerit nibh posuere ut. Praesent rhoncus volutpat lorem, vehicula ultricies erat convallis eget. Nunc feugiat auctor mauris, non dapibus nulla dapibus in. Phasellus vitae egestas sapien, in efficitur eros. Maecenas iaculis sed velit at vehicula. Suspendisse vulputate justo placerat augue feugiat molestie. Vivamus eu feugiat augue, quis pharetra eros."
-        />
-      </Card>
     </>
   );
 };
